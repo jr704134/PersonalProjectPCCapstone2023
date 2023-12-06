3ded2f872267530f5b563eea0b3e0039f7ff406b
John Ramirez <businessjuando@gmail.com>
2023-12-05 21:24:36 -0600

    made CustomModel again

---
+++ Models/CustomModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalProjectPCCapstone2023.Models
+{
+    public class CustomModel
+    {
+        public Merchandise Merch { get; set; }
+
+        public IList<CategoryIList> Category { get; set; }
+    }
+}
